a9a0baa255550d45a976d5be882e1e96b8f1ed85
Tedhoon <gt0305@likelion.org>
2020-03-24 03:07:59 +0900

    [add] main app add & api key test & create superuser

--- PusanCoronaProject/PusanCoronaProject/settings/base.py
+++ PusanCoronaProject/PusanCoronaProject/settings/base.py
@@ -85,8 +85,25 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'pnustatic')
 
-STATIC_URL = '/pnustatic/'
+# 배포시 10분에 한번씩 아래의 함수를 실행하여 데이터베이스를 자동으로 업데이트 합니다.
+# 데이터베이스에 환자 동선 정보가 업데이트 되었을 경우 아래에 설정한 이메일로 경보 알람이 갑니다.
+CRONJOBS = [
+    ('*/10 * * * *', 'main.views.get_mask_stores', '>> /home/ubuntu/mask_stores.log'),
+    ('*/10 * * * *', 'main.views.get_status', '>> /home/ubuntu/statistics.log'),
+    ('*/10 * * * *', 'main.views.get_patients', '>> /home/ubuntu/patients_crawl.log'),
+]
 
+# 경보 메일을 받기 위한 이메일 설정입니다.
+# 사용할 이메일의 SMTP 서버 사용을 허용하고 추가하셔야 합니다. 아래 링크는 Gmail 사용 기준 SMTP 서버 사용 가이드입니다.
+# https://velog.io/@ground4ekd/django-send-mail#gmail-smtp-%EC%84%9C%EB%B2%84
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = '경보 메일을 받을 이메일 입력'
+EMAIL_HOST_PASSWORD = '이메일 비밀번호 입력'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
