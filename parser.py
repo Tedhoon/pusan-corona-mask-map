131a0ac59e2821df429128312598c44a7b631de6
Tedhoon <gt0305@likelion.org>
2020-03-30 21:30:10 +0900

    [mdf] \ xa0 유니 코드(공백) 변환 처리

--- parser.py
+++ parser.py
@@ -64,7 +64,7 @@ def temp_patient_parser():
 
 # temp_patient_parser()
 
-import re
+import re,unicodedata
 
 def pusan_temp_patient_parser():
     url = "http://www.busan.go.kr/corona19/index"
@@ -87,51 +87,22 @@ def pusan_temp_patient_parser():
         temp_path=list()
         for i in path_info:
             if i.text !="" and i.text[0] != "※":
-                
-                cleanr =re.compile('<.*?>')
-                cleantext = re.sub(cleanr, '', i)
-                temp_path.append(cleantext)
-        # print(temp_pa)
-        # print("[Age] : "+ personal_info[0][-6::])
-        # print("[Confirmed Date] : "+ patient_info[4].text)
-        # print("[Current Status] : "+ patient_info[3].text)
-        # print("[Gender] : "+ personal_info[1].replace(' ',''))
-        # print("[ID] : "+ personal_info[0][:-8])
-        # print("[Paths] : ")
-        print(temp_path)
-        # print("[Region] : "+ personal_info[2].replace(' ','').replace(')',''))
-     
-        print("--------------")
+                temp_text = re.sub(r'\xa0', ' ', i.text)
+                temp_path.append(temp_text)
+    
+        print("[Age] : "+ personal_info[0][-6::])
+        print("[Confirmed Date] : "+ patient_info[4].text)
+        print("[Current Status] : "+ patient_info[3].text)
+        print("[Gender] : "+ personal_info[1].replace(' ',''))
+        print("[ID] : "+ personal_info[0][:-8])
+        print("[Paths] : ")
+        pp = pprint.PrettyPrinter(indent=1)
+        pp.pprint(temp_path)
+        print("[Region] : "+ personal_info[2].replace(' ','').replace(')',''))
+
     print("성공")
 
-    patients_rows = list(table.select("tbody tr"))[::-1]
-
-    total_patients = int(len(patients_rows) / 2)
-    # print(total_patients)
-    patients = {}
-
-    patient_num = 1
-    for num, row in enumerate(patients_rows):
-        # print(patient_num)
-        if num % 2 == 0:
-            patients[patient_num] = {}
-            patients[patient_num]["Paths"] = [path.text for path in row.select('.corona-move li')]
-        else:
-            informations = [info.text for info in row.select('td')]
-            # print(informations)
-            patients[patient_num]["ID"] = informations[0]
-
-            patient_details = informations[1].split("/")
-            patients[patient_num]["Gender"] = patient_details[0]
-            patients[patient_num]["Age"] = patient_details[1]
-            patients[patient_num]["Region"] = patient_details[2]
-
-            patients[patient_num]["Confirmed Date"] = informations[3]
-            patients[patient_num]["Current Status"] = informations[4]
-
-            patient_num += 1
-    pp = pprint.PrettyPrinter(indent=1)
-    pp.pprint(patients)
+  
 pusan_temp_patient_parser()
 
 # 예시입니당
