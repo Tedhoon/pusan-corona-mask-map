b6a4dad0f0f1f22324d2da9952a2175f33736b5b
Tedhoon <gt0305@likelion.org>
2020-04-19 15:37:47 +0900

    [mdf] pusan converting :recycle:

--- PusanCoronaProject/src/cron.py
+++ PusanCoronaProject/src/cron.py
@@ -9,14 +9,25 @@ def get_ulsan_mask_stores():
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'
     }
     gu_list = [
-        "울산광역시 울주군",
-        "울산광역시 남구",
-        "울산광역시 북구",
-        "울산광역시 동구",
-        "울산광역시 중구",
+        "부산광역시 강서구",
+        "부산광역시 금정구",
+        "부산광역시 남구",
+        "부산광역시 동구",
+        "부산광역시 동래구",
+        "부산광역시 부산진구",
+        "부산광역시 북구",
+        "부산광역시 사상구",
+        "부산광역시 사하구",
+        "부산광역시 서구",
+        "부산광역시 수영구",
+        "부산광역시 연제구",
+        "부산광역시 영도구",
+        "부산광역시 중구",
+        "부산광역시 해운대구",
+        "부산광역시 기장군",
     ]
 
-    ulsan_mask_stores = {
+    pusan_mask_stores = {
         'stores': [],
         'count': 0,
     }
@@ -26,10 +37,10 @@ def get_ulsan_mask_stores():
         }
         mask_json = json.loads(requests.get(url, params=params, headers=headers).text)
         # pprint(mask_json)
-        ulsan_mask_stores['count'] += mask_json['count']
-        ulsan_mask_stores['stores'] += mask_json['stores']
+        pusan_mask_stores['count'] += mask_json['count']
+        pusan_mask_stores['stores'] += mask_json['stores']
 
     print("I GET DATA!")
-    return ulsan_mask_stores
+    return pusan_mask_stores
 
-get_ulsan_mask_stores()
+get_pusan_mask_stores()
--- parser.py
+++ parser.py
@@ -21,7 +21,7 @@ import pprint
 
 
 def temp_patient_parser():
-    url = "http://www.ulsan.go.kr/corona.jsp"
+    url = ""
     headers = {
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'
     }
